0a8b68c75e62db13db2f560609e3d70d50154669
Clair-Hawkes <hawkesj7@gmail.com>
2022-06-08 15:23:42 -0700

    Source Files

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+from stories import silly_story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
